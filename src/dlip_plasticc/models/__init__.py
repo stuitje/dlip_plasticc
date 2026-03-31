8382f7b54f63d849a09f7f992a1d0a960c92f02e
Karen Stuitje <e.k.e.stuitje@student.rug.nl>
2026-04-01 01:35:23 +0200

    update init with new hybrid model

--- src/dlip_plasticc/models/__init__.py
+++ src/dlip_plasticc/models/__init__.py
@@ -1,5 +1,13 @@
 from .cnn import CNNClassifier
 from .transformer import TransformerClassifier
 from .sequence_cnn import SequenceCNNClassifier
+from .hybrid_transformer import HybridTransformerClassifier
+from .transformer_gbm import TransformerGBMClassifier
 
-__all__ = ["CNNClassifier", "TransformerClassifier", "SequenceCNNClassifier"]
+__all__ = [
+    "CNNClassifier",
+    "TransformerClassifier",
+    "SequenceCNNClassifier",
+    "HybridTransformerClassifier",
+    "TransformerGBMClassifier",
+]
