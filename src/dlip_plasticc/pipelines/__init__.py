a4c120b6062a86cb8629c71cb6c84c75abecbc20
Karen Stuitje <e.k.e.stuitje@student.rug.nl>
2026-03-31 12:23:30 +0200

    add init for pipelines

---
+++ src/dlip_plasticc/pipelines/__init__.py
@@ -0,0 +1,35 @@
+from .predict import (
+    combine_predictions,
+    load_feature_chunk,
+    predict_partial_from_dataset,
+    predict_partial_from_feature_chunks,
+    save_chunk_predictions,
+)
+from .score import (
+    align_truth_and_predictions,
+    normalize_rows,
+    score_flat,
+)
+from .blend import (
+    align_prediction_frames,
+    blend_predictions,
+    grid_search_blend_weight,
+    read_prediction_hdf,
+    save_blended_predictions,
+)
+
+__all__ = [
+    "combine_predictions",
+    "load_feature_chunk",
+    "predict_partial_from_dataset",
+    "predict_partial_from_feature_chunks",
+    "save_chunk_predictions",
+    "align_truth_and_predictions",
+    "normalize_rows",
+    "score_flat",
+    "align_prediction_frames",
+    "blend_predictions",
+    "grid_search_blend_weight",
+    "read_prediction_hdf",
+    "save_blended_predictions",
+]
